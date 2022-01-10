9eaed626747e1714ce30e9eeb8647b3900b16fb4
stttfrit <thomas.fritz@student.hs-anhalt.de>
2022-01-10 15:05:21 +0100

    added bad index.html

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,14 @@
 require('dotenv').config()
 const http = require("http");
-
+const fs = require("fs");   // for reading files
 const host = process.env.HOST;
 const port = process.env.PORT
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(fs.readFileSync(__dirname + "/pages/index.html"));
 });
 
 server.listen(port, host, () => {
